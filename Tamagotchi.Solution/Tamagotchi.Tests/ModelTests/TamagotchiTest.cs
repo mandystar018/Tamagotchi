0af5894d6d83545f60074a422bfb710d36cb1389
Amanda Otero <mandystar018@gmail.com>
2020-12-16 10:52:01 -0800

    Add Files and folders, MVC files and test
    co-authored-by: Patrick Osten <posten.coding@gmail.com>

---
+++ Tamagotchi.Solution/Tamagotchi.Tests/ModelTests/TamagotchiTest.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tamagotchi.Tests
+{
+  [TestClass]
+  public class Pets
+  {
+    [TestMethod]
+    public void Pet_CreateAnInstanceOfPet_Pet
+    {
